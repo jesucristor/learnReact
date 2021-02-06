0ca2c20ab8659dc2af30e854190e9e086645ca8c
jesus <je_90us@hotmail.com>
2021-02-05 23:59:46 -0300

    Created part1 folder and initialize the app

---
+++ Basics/part1/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const MyName = () => {
+  const [name, setName] = useState('')
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
+  return (
+    <div>
+      <h1>My name is: {name}</h1>
+      <input type='text' value={name} onChange={handleChange} />
+    </div>
+  )
+
+}
+
+
+export default MyName;
